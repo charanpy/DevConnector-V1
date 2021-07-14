544f0617399d4e7e7d42355be0d20ccea4ef4067
charanpy <durgicharan81@example.com>
2021-07-14 20:12:54 +0530

    test

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,9 @@
-const express = require("express")
-
+const express = require('express');
+const cors = require('cors');
 const app = express();
 
-const connectDB = require("./config/db")
+app.use(cors());
+const connectDB = require('./config/db');
 const PORT = process.env.PORT;
 connectDB();
 const user = require('./routes/api/user');
@@ -10,15 +11,13 @@ const auth = require('./routes/api/auth');
 const profile = require('./routes/api/profile');
 const post = require('./routes/api/post');
 //
-app.use(express.json({ extended: true }))
+app.use(express.json({ extended: true }));
 //Routers
 app.use('/api/users', user);
 app.use('/api/auth', auth);
 app.use('/api/profile', profile);
 app.use('/api/post', post);
 
-
-
-app.listen(PORT || 3000, () => {
-            console.log('Started')
-})
+app.listen(PORT || 3001, () => {
+  console.log('Started');
+});
